91a384587c5a2490acb1324aa606e2f86d4696f9
Petras Vestartas <petrasvestartas@gmail.com>
2021-08-15 10:41:28 +0100

    Update Export.h

--- PInvokeON/Export.h
+++ PInvokeON/Export.h
@@ -10,23 +10,6 @@
 
 #define PINVOKE extern "C" __declspec(dllexport)
 
-
-
-// Windows build
-//#if defined (_WIN32)
-//#if defined (SAMPLELIBRARY_DLL_EXPORTS)
-#define PINVOKE_CPP_CLASS __declspec(dllexport)
-#define PINVOKE_CPP_FUNCTION __declspec(dllexport)
-#define PINVOKE_C_FUNCTION extern "C" __declspec(dllexport)
-//#else
-//#define SAMPLELIBRARY_CPP_CLASS __declspec(dllimport)
-//#define SAMPLELIBRARY_CPP_FUNCTION __declspec(dllimport)
-//#define SAMPLELIBRARY_C_FUNCTION extern "C" __declspec(dllimport)
-//#endif // SAMPLELIBRARY_DLL_EXPORTS
-//#endif // _WIN32
-
-#define deletePtr(ptr, isArray) if (isArray) {delete[] arr;} else {delete arr;}
-
 PINVOKE void ReleaseInt(int* arr, bool isArray);
 PINVOKE void ReleaseFloat(float* arr, bool isArray);
 PINVOKE void ReleaseDouble(double* arr, bool isArray);
